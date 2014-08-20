8ab0d6d6a07211e82090b5dbe51448c43f59763f
festaans <stephanus@glio.co.za>
2014-08-20 16:45:14 +0200

    Solver

---
+++ challenge3200.js
@@ -0,0 +1,106 @@
+var challenge = 'hpevfwqjmjryhemuqjoiatpjmddxdjwzskdcfgdtbmkbcxrnmjuoyddnqwluimjwvguxehszxzvbmufq\nlrepncxxbrrzxnzmkoyhrjcstvfazyhrhgssximjdfcmdjusylfkwbedyrsxovrmvjzaljfjmywpfnjg\nisoqbdyspgzlcmdjmhbpxhzvvhckidzuwzkauffsujmcrhvgeqvasjakgtzlxkthjqwxypmsovjbfshr\nrxtdvkmbyhejoeydnrdowuwhgmbvxmpixyttglsjgmcoqbberssfjraaqfrkmebsozsjfnubhktbbai_\nvxbifbofyednnutmxtisvfsktbqfijfzdjoqybuohtztysqelaqyixyaiolbgwylwfisfwubivuoablx\nsmrqggedwyiqvseevwbcxcfjttdbweedcjgnsorizflsjtmltcoaynsrsupavqwcyzhgiplwkohlhrai\nnazaacvuqblpbzimgoxirejbshnbmdtgsbvlhpnugggencjaczqqiwixrwiyobmlkbwdlwcioqmjhoac\ndvcqdypxeichmgywocbcafumthdqrbjnpgnnmaasxiaxxfymcyiuqduztqneodstbcnjpeebgxgosoyd\nvpzlqjuroebbehafsemanwprhwkircuhlgcftqsjdusrqetbthxclfokpdlspxzuvhxpbeqqbfpqffsg\nyilqltfxrmtimcugytazkerhcfnirtavcnmfdyictlncwttkmxyfhgejygfefqrjknuqsfldmjmwjdfq\nsicfrzbfazchdgznekwmhridelcejnkmcgmpgtihbwmplrtrrefoyhyzxpjjlkabbbgspeokzhpjxsvp\nfjmdsoripvfrgyzxodoeirwwdaofdmwqrqyvdijlfqyzfspdoyrhewxbpufdqcpqdolkmrnvedixzpfd\nakggkslxcrjbrmnynviihbkzaqqffkkcgwjbettexhlwlasdfjnslwsmnclhafvebxxfdozsjtdvobik\nrrsuysujwliobagobxmlyxjeltwzwxpyrnkdxfemotfncyriaycyfemygjmpboocgtsvttqntegvleyn\nwgpjhyyysbltoxljsascsngbgfqmpzgpejzlmdkjzzlfxvagyrasmpzqntgqsvyqjugkhbrbkiqewlyf\ntvsq_______znp_____xkwt______wef______tz______kfc_______ha_______pn__lmg__iakrbt\niyfi__uojrxvx__tps__fp__pfpndbi__ggpalde__wmd__kn__ifiadob__hdljdbd__zl__whlwilt\nbcmt__haagmjg__dwx__oh__utnzudq__xstxxyc__vly__mr__viilzav__swosyvc__i__hnaqxyev\njykc__wyfoyir__ewp__ij__mrdavxl__tcdtxqy__fnr__cf__mrkepwj__djhrsau____lhefqxgmu\nzdgf______tjg__fip__mi__b____xc__vjvhpqy______vff_____wuup_____kqct___htiggvvpet\nyvco__pqbrlox__ayj__af__dnn__kx__mlitytx____jauna__kncmiym__dlwushk____gjptzccgc\nnntt__hfqyxzi__eqn__vz__hlh__we__dtfkfvf__g__litm__zeqjtdl__bkdapxs__o__oxeouwer\nbfjr__ipcqmop__kec__ip__icc__ci__vpxxueu__eq__sau__nhheydy__efqkdgq__us__pzlndhk\nhdmk__cmfvzwcb_____xdka______trj______yj__xpi__he_______nb_______by__rrn__tvxvig\njfpseyjjbrrtsfnmbrokdqtfzhhdtbhtvpiyshmvcqaypfxcvbgvbvwrkanjfcsjnanmktkwimnvynuk\ncmgtqmovkrdmfuduqvbqydagsttictcnsrhfrpoebcehdzhjamykqpjtktufcvokljjijjsrivyhxtgw\nojgoujyhmekzsoczwlqnruwcuhudgfaijzrkewzgjvorsmabpcdmurctwjrddcnkmfvabjwlbqssihdy\nbgfqchqdvjcsdllrlwmyikuvthguzfbgocaeqktvbcapzdcfjphqnhundtljqjeyfrkjspfvghqddxwx\nidtjjkctrkfcjmdpqyvavqbntpmkkuswfgbgalrysjfnzezjjscahoodjjelavydefzjmhsqfufsexlv\nvzziymsyqrcvhsrxjnysioswvjlqdbnwgyjlanmhzkbygkptycdoifsibytbrixggjeiepaybzxhvfsy\nayeptgpxbhhfkkpromhjykfxnujorlzcmkcmvvgmveyfkgiwgosznfpmbhixsakxfkuxhwcgularehpa\nguquulrjllxmkfzgnchrxzcfdklytpfnezergkwkhgalqlvdhkdgulgfaxtybqttcjtlgmfwaymaxlwa\nspyrboibwkzzbtgigyswbtpwxgphcmkfpmvbfjimnxctinqssshofhlvlpqcwiuacjyxyqmvaibezofv\natyhpqvjubgcwqeoytloypjphoxeimumuvswxkgamodoxiciwmgxvsenkgdhttzlenjbszrksopicjcj\nnvsosrapkfilwsaoptdavlfglioqpwoqskbgikksnnuzvmxyrtrbjouvgokxgbnwxnivtykvhjkaydsk\nzoowbhjrlojgeecdoggqqtomcdgrjzmlkhubyaewwtrlyutsptdrrigopueicoganyasrjeaiivzairu\nlklovyrpckwpowprxtvhaeivpudfchxbwvtosmivpcsesbzpsynxitlisuifuehceonjeydljzuzpsgj\nllcywoxbblitscquxiykcjxhsgkbhfhfrshsrpyrcaetahuwbeybvlvkthxydkapxlfikdwudjkmjjsa\nzajxpuikiqwsifhldfovqoycwmtlmcaycirhcehxnpfadrgyaogpcmomcgtmacnvbwfnimaqqvxijcbp\nmckwimloiinindfuakqjmpyjisxnbybtywhymnkdoyiphijzelmrazplgfcmcsjiovxqdxmuqulzklgx';
+var str = 'abcdefghijklmnopqrstuvwxyz_';
+var candidate = {value: 0};
+challenge = challenge.split('\n').join(''); //remove line breaks
+function findAllInstances() {
+    var i;
+    for (i = str.length - 1; i >= 0; i--) {
+        if (challenge.indexOf(str[i]) != challenge.lastIndexOf(str[i])) {
+            var everyinstanceof = [];
+            var reg = new RegExp(str[i],"g");
+            var m;
+            while (m = reg.exec(challenge)) {
+              everyinstanceof.push(m.index);
+            };
+            buildPairs(everyinstanceof);  
+        }
+    };
+}
+function buildPairs(everyinstanceof) {
+    var i;
+    for (var i = everyinstanceof.length - 1; i >= 0; i--) {
+        var valueOfThis = everyinstanceof[i];
+        var index = everyinstanceof.indexOf(valueOfThis);
+        if(index >= 0 && index < everyinstanceof.length - 1) {
+            var nextItem = everyinstanceof[index + 1];
+            if (nextItem != undefined) {
+                var diff = nextItem - valueOfThis;
+                pairs.push({first: valueOfThis, last: nextItem, value: diff});
+            };
+            var nextItemPlus = everyinstanceof[index + 2];
+            if (nextItemPlus != undefined) {
+                var difftwo = nextItemPlus - valueOfThis;
+                pairs.push({first: valueOfThis, last: nextItemPlus, value: difftwo});
+            };
+        };
+
+    };
+}
+function findUnique() {
+    var i;
+    for (var i = pairs.length - 1; i >= 0; i--) {
+        var pair = pairs[i];
+        var part = challenge.substring(pair.first+1 , pair.last);
+        if (part.length === 0) {
+            uniquePairs.push(pair);
+        } else {
+            //check for pairs inside            
+            var allUnique = true;
+            var j;
+            for (var j = part.length - 1; j >= 0; j--) {
+                if (part.indexOf(part[j]) != part.lastIndexOf(part[j])) {
+                var allUnique = false;
+                }
+            };
+            if (allUnique === true) {
+                uniquePairs.push(pair);
+            }; //end if allUnique
+        } //end if
+    };
+    //console.log(uniquePairs);
+}
+function findCandidate() {
+    for (var i = uniquePairs.length - 1; i >= 0; i--) {
+        var uniquePair = uniquePairs[i];
+        var largestValue = candidate.value;
+        if (uniquePair.value > largestValue) {
+           candidate = uniquePair;
+        } else if (uniquePair.value == largestValue) {
+            //if curent pair's i1 is lower than the current canditate's make it the new candidate
+            if (uniquePair.first < candidate.first) {
+                candidate = uniquePair;
+            }
+        };
+    };
+}
+function removeAndAdd() {
+    if (candidate.first === undefined) {
+        clearInterval(solve);
+        chopTheTail();
+    } else {
+        var indexFirst = candidate.first;
+        var indexLast = candidate.last;
+        challenge = challenge.substring(-1, (indexFirst)) + challenge.substring((indexFirst + 1), (indexLast)) + challenge.substring((indexLast + 1)) + challenge.charAt(indexFirst);
+    }
+};
+function chopTheTail() {
+    var underscore = /_/g;
+    var hasUnderscore = underscore.test(challenge);
+    if (hasUnderscore == true) {
+        var endOfBody = challenge.indexOf("_");
+        challenge = challenge.substring(-1, (endOfBody));
+    };
+    console.log(challenge);
+};
+function solver() {
+    pairs = [];
+    uniquePairs = [];
+    candidate = {value: 0};
+    findAllInstances();
+    findUnique();
+    findCandidate();
+    removeAndAdd();
+}
+var solve = setInterval(function () {
+    solver();
+}, 0);
